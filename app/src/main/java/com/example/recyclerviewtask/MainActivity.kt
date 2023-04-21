0f7a82812dedb645b4f93a885a187aa4214a06f7
ranjithdurai098 <ranjithbrjith098@gmail.com>
2023-04-21 22:59:35 +0530

    update

--- app/src/main/java/com/example/recyclerviewtask/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewtask/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.recyclerviewtask
 
+import android.app.AlertDialog
 import android.content.Context
 import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewtask.databinding.ActivityMainBinding
@@ -17,12 +13,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity(), CoroutineScope {
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Main
+    override val coroutineContext = Dispatchers.Main
     private lateinit var binding: ActivityMainBinding
+    private val api = retrofit.init().create(ApiInterface::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -32,54 +28,56 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
         }
         binding.recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         doSomethingWithInternet(this)
-        val api = retrofit.init().create(ApiInterface::class.java)
-        launch {
-            val response = api.login()
-            if (response.isSuccessful) {
-                binding.recyclerView.adapter = Adapter(response.body()!!, this@MainActivity)
-            } else {
-                showErrorDialog()
-            }
-        }
     }
 
     private fun showErrorDialog() {
-
+        AlertDialog.Builder(this)
+            .setTitle("No internet connection")
+            .setMessage("Please check your internet connection and try again.")
+            .setPositiveButton("Retry") { _, _ ->
+                doSomethingWithInternet(this)
+            }
+            .setNegativeButton("Exit") { _, _ ->
+                finish()
+            }
+            .setCancelable(false)
+            .show()
     }
 
     fun isInternetAvailable(context: Context): Boolean {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val network = connectivityManager.activeNetwork ?: return false
-        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
-        return when {
-            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-            else -> false
-        }
+        val connManager = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkCapabilities = connManager.getNetworkCapabilities(connManager.activeNetwork)
+        return networkCapabilities != null
     }
 
-    // To use this function, call it with the context of your activity or fragment.
-// If the internet is available, the function will return true.
-// If the internet is not available, the function will return false and show a toast message to the user.
     fun doSomethingWithInternet(context: Context) {
         if (isInternetAvailable(context)) {
-            // Do something that requires internet
+            launch {
+                try {
+                    val response = api.login()
+                    if (response.isSuccessful) {
+                        binding.recyclerView.adapter = Adapter(response.body()!!, this@MainActivity)
+                    } else {
+                        showErrorDialog()
+                    }
+                } catch (e: NoInternetException) {
+                    showErrorDialog()
+                }
+            }
         } else {
-            Toast.makeText(context, "No internet connection", Toast.LENGTH_SHORT).show()
+            showErrorDialog()
         }
     }
 }
 
 object retrofit {
-
     fun init(): Retrofit {
         val baseUrl = "https://jsonplaceholder.typicode.com"
-        return Retrofit.Builder().baseUrl(baseUrl)
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
-            // we need to add converter factory to
-            // convert JSON object to Java object
             .build()
     }
 }
+
+class NoInternetException(message: String) : Exception(message)
